cc9789b00500d9802e3d49d8d278de57a9e9530d
CYCheng4A10 <107162728+CYCheng4A10@users.noreply.github.com>
2022-09-08 11:11:24 +0800

    Update D101.py

--- D101.py
+++ D101.py
@@ -1,5 +1,5 @@
-n = input()
-firstDigit = n[0]
+n = input()                
+firstDigit = n[0]          #find the first string
 if firstDigit in ('2', '3'):
     print('Fixed')
 elif firstDigit in ('5','6','9'):
